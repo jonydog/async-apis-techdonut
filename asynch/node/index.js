1918f44f25a904ead271f6a0c026b875f36fedf1
Joao Pinto <joao.pinto@talkdesk.com>
2020-04-23 17:55:56 +0100

    change node port to 9090

--- asynch/node/index.js
+++ asynch/node/index.js
@@ -5,10 +5,11 @@ const server = http.createServer((req, res) => {
     console.log(req.url)
 	if ((req.url === '/hello') && req.method === 'GET') {
         setTimeout(
-            function(){
+            ()=>{
                 res.statusCode = 200
                 res.end('Hello')
-            }.bind(this),1500
+            },
+            1500
         )
         return
 	}
@@ -16,7 +17,7 @@ const server = http.createServer((req, res) => {
 	res.end('Not found')
 })
 
-const port = process.env.PORT || '8080'
+const port = process.env.PORT || '9090'
 server.listen(port,()=>{
     console.log(`Server listening on port ${port}`)
 })
